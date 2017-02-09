1050006be33deed60f9495b52d2447b1e1d897a3
izelenev <the_lenev@mail.ru>
2017-02-09 10:07:34 +0300

    Create repository

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <link rel="stylesheet" href="\css\style.min.css">
+    <script type="text/javascript" src="\js\jquery-1.12.4.js"></script>
+    <script type="text/javascript" src="\js\scripts.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="title">
+        </div>
+        <form id="form" class="col-sm-7 text-center" action="/" method="post">
+        </form>
+    </div>
+</body>
+</html>
