f2512e5fad9284e0a2e614207c2d5fb044b0af2f
izelenev <the_lenev@mail.ru>
2017-02-09 10:26:05 +0300

    Add js script for form and exchanges through ajax

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(function() {
-	
+	$('.group button').on('click', function(){
+		console.log('submit');
+		var formData = new FormData(form);
+        $.ajax({
+            url: '/ajax/game.php',
+            processData: false,
+			contentType: false,
+            type: 'post',
+            data: formData,
+            success: function (data) {
+				var arr_num = data.split(';');
+            	$('#boys_candy').html(arr_num[0]);
+				$('#girls_candy').html(arr_num[1]);
+            }
+        });
+	});
+	$('.group.active button').on('click', change_active);
+	function change_active(){
+		console.log('count');
+		$('.group.disabled').removeClass('disabled').addClass('active').find('select').attr('disabled', 'disabled').parents('.group').find('.btn-info').removeAttr('disabled')
+			.off('click', change_active)
+			.on('click', change_active);
+		$(this).attr('disabled', 'disabled').parents('.group').removeClass('active').addClass('disabled').find('select').removeAttr('disabled')
+			.off('click', change_active);
+	}
 });
