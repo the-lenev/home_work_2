08b91da4b78a7efe282482b14b7f442aa3c29f94
izelenev <the_lenev@mail.ru>
2017-02-09 10:41:48 +0300

    Add logic in game.php

--- ajax/game.php
+++ ajax/game.php
@@ -1,1 +1,42 @@
 <?
+require_once 'create.php';
+
+//var_dump($_POST);
+$count = 1;
+if ($count%2 == 0) {
+    search($boys);
+    add($girls,($count-1)/2);
+} else {
+    search($girls);
+    add($boys,($count/2)-1);
+}
+$count++;
+if ($count == 11) {
+    $count = 1;
+}
+
+echo candy_num ($boys).';'.candy_num ($girls);
+
+function search($children) {
+    foreach ($_POST as $name => $candy) {
+        if ($candy != 0) {
+            foreach ($children as $child) {
+                if ($child->name == $name) {
+                    if ($child->candy - $candy >= 0) {
+                        $child->candy -= $candy;
+                        return $child->candy;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
+function add($ch, $index){
+    foreach ($_POST as $candy) {
+        if ($candy != 0) {
+            $ch[$index]->candy += $candy;
+        }
+    }
+}
